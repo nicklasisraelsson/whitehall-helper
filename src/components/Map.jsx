a610018e42ec26df8867b4e716ddbd7701832c85
Nicklas Israelsson <nicklas.israelsson@episerver.com>
2018-11-01 17:00:27 +0100

    Make the map scale to viewport

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,35 @@
 import React from "react"
 import mapImage from "../data/whitehall-map.png";
 
-const drawPosition = (ctx, position) => {
-    ctx.beginPath();
-    ctx.arc(position.x, position.y, 15, 0, Math.PI * 2, true);
-    ctx.fillStyle = position.keep ? "green" : "red";
-    ctx.fill();
-}
-
 export default class Map extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            image: null
+            image: null,
+            scale: 1
         };
     }
 
     componentDidMount() {
         const image = new Image();
+        this.setState({image});
         image.onload = () => {
             this.updateCanvas();
         }
         image.src = mapImage;
-        this.setState({image});
+        this.setScale();
+        window.addEventListener("resize", () => {
+            this.setScale();
+        });
+    }
+
+    setScale() {
+        let scale = 1;
+        if (window.innerWidth < 1024) {
+            scale = window.innerWidth / 1024;
+        }
+        this.setState({scale});
     }
 
     componentDidUpdate() {
@@ -32,12 +38,22 @@ export default class Map extends React.Component {
 
     updateCanvas() {
         const ctx = this.refs.canvas.getContext("2d");
+
+        ctx.canvas.width = 1024*this.state.scale;
+        ctx.canvas.height = 1024*this.state.scale;
         ctx.globalAlpha = 1;
-        ctx.drawImage(this.state.image, 0, 0);
+        ctx.drawImage(this.state.image, 0, 0, 1024*this.state.scale, 1024*this.state.scale);
         ctx.globalAlpha = 0.5;
         this.props.positions.forEach(position => {
-            drawPosition(ctx, position);
+            this.drawPosition(ctx, position);
         });
+    }
+
+    drawPosition(ctx, position) {
+        ctx.beginPath();
+        ctx.arc(position.x*this.state.scale, position.y*this.state.scale, 15*this.state.scale, 0, Math.PI * 2, true);
+        ctx.fillStyle = position.keep ? "green" : "red";
+        ctx.fill();
     }
 
     render() {
